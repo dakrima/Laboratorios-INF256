196afb70435435f108784c0cec3ce347df1d4525
David <103002074+dakrima@users.noreply.github.com>
2025-08-19 17:20:51 -0400

    feat: modificaciones no terminadas PORQUE SALI ayayayayaiiii

--- Laboratorio 1/servicio1.py
+++ Laboratorio 1/servicio1.py
@@ -2,10 +2,22 @@
 import socket
 from datetime import datetime
 
-#holaa probando
-
 cliente_TCP = ("localhost", 9000)
 servidor_TCP = ("", 9002)
+palabra_finalizar = "FINAL"
+
+def finalizar(text: str) -> bool:
+    msg_partes = text.strip().split("-", 1)
+    if len(msg_partes) == 2 and msg_partes[1].strip().upper() == palabra_finalizar:
+        return True
+    return len(text.split("-", 3)) < 4
+
+def mandar_a_servicio_2(text: str):
+    data = (text.rstrip() + "\n").encode("utf-8")
+    c = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    c.connect(cliente_TCP)
+    c.sendall(data)
+    c.close()
 
 min_largo = int(input("[S1] Largo mínimo: ").strip())
 first = input("[S1] Palabra inicial: ").strip()
